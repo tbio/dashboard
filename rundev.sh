2fee9469588bd3c6df8c9ebb9068e87201ee57f4
AndresRoot <strike1609@gmail.com>
2014-09-28 17:37:14 -0500

    bugfix in rundev file

--- rundev.sh
+++ rundev.sh
@@ -4,6 +4,6 @@ LOGFILE=/webapp/tbio/logs/gunicorn.log
 LOGDIR=$(dirname $LOGFILE)
 NUM_WORKERS=3
 # user/group to run as                                                                                                                                                       
-cd /webapp/cardio/
+cd /webapp/dashboard/
 . ../bin/activate
 exec ./manage.py runserver 0.0.0.0:8000
